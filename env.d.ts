c951a91afe9ba4c0f9a1757f9275b129a2eba1d9
alanchoi00 <alanchoi.uni@gmail.com>
2025-02-08 17:12:11 +0800

    feat: Add RECAPTCHA_SECRET_KEY and NEXT_PUBLIC_RECAPTCHA_SITE_KEY to environment variable declarations

--- env.d.ts
+++ env.d.ts
@@ -3,5 +3,7 @@ declare namespace NodeJS {
         GOOGLE_SHEETS_API_URL: string;
         GOOGLE_SHEETS_API_TOKEN: string;
         GOOGLE_SHEETS_SPREADSHEET_ID: string;
+        RECAPTCHA_SECRET_KEY: string;
+        NEXT_PUBLIC_RECAPTCHA_SITE_KEY: string;
     }
 }
