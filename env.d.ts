726edd19c2256a234d228ad029e140fac7af5d91
alanchoi00 <alanchoi.uni@gmail.com>
2025-02-08 17:11:32 +0800

    feat: Remove NEXT_PUBLIC_GOOGLE_SHEETS_CLIENT_TOKEN from environment variable declarations

--- env.d.ts
+++ env.d.ts
@@ -2,7 +2,6 @@ declare namespace NodeJS {
     export interface ProcessEnv {
         GOOGLE_SHEETS_API_URL: string;
         GOOGLE_SHEETS_API_TOKEN: string;
-        NEXT_PUBLIC_GOOGLE_SHEETS_CLIENT_TOKEN: string;
         GOOGLE_SHEETS_SPREADSHEET_ID: string;
     }
 }
