45aecb6a0ad5e7097532a31d203426509209fd21
alanchoi00 <alanchoi.uni@gmail.com>
2025-02-09 23:08:42 +0800

    feat(form): Refactor layout structure to include head element and improve styling

--- src/app/sumobots/2025/(form)/layout.tsx
+++ src/app/sumobots/2025/(form)/layout.tsx
@@ -7,14 +7,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="antialiased">
-      <body>
-        <main className="font-main w-full bg-black text-2xl text-white">
-          <Script
-            strategy="beforeInteractive"
-            src={`https://www.google.com/recaptcha/enterprise.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
-          />
-          {/* <ReCaptcha/> */}
+    <html lang="en" className="antialiased no-scrollbar overflow-y-scroll">
+      <head>
+        <Script
+          strategy="beforeInteractive"
+          src={`https://www.google.com/recaptcha/enterprise.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
+        />
+      </head>
+      <body className="font-main w-full bg-black text-2xl text-white">
+        <main>
           {children}
         </main>
       </body>
