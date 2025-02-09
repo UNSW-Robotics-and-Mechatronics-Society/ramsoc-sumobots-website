e707c4e7482675d9bb8a592d63258eca29687bf2
alanchoi00 <alanchoi.uni@gmail.com>
2025-02-09 23:08:22 +0800

    feat(Navbar): Update navbar height and styling for improved layout

--- src/app/sumobots/2025/(form)/_components/Navbar.tsx
+++ src/app/sumobots/2025/(form)/_components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Navbar() {
   return (
-    <nav className="sticky left-0 top-0 z-50 h-24 w-full text-xl mb-[-6rem]">
+    <nav className="h-[10vh] min-h-20 w-full text-xl">
       <div className="flex h-full w-full items-center justify-center">
         <Link className="hover:underline underline-offset-4 px-3 py-2" href="/sumobots/2025">
           SUMOBOTS 2025
