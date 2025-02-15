74aa3a9e923def21f6f24cf22f4ccba44cbd0e18
alanchoi00 <alanchoi.uni@gmail.com>
2025-02-15 16:43:51 +0800

    feat(home): Enhance layout structure and improve component organization for better readability

--- src/app/sumobots/2025/(home)/page.tsx
+++ src/app/sumobots/2025/(home)/page.tsx
@@ -19,51 +19,53 @@ export default function Page() {
           </span>
         </h1>
       </div>
-      <div className="min-h-screen w-full pt-12 bg-black/30">
-        <section id="about" className="container">
-          <h2 className="font-display col-span-full">What is SUMOBOTS.</h2>
-          <div className="col-span-full flex flex-col-reverse items-center md:flex-row gap-8 mt-8">
-            <div className="md:w-[60%]">
-              <p>
-                Sumobots is a sport in which two robots attempt to push each other
-                out of a circle. You will be competing in a team of 1-6 people to
-                build a robot that can defeat other bots. The competition is
-                divided into the basic and the advanced stream, depending on your
-                experience.
-              </p>
-              <Link
-                className="button mt-8"
-                href={"/sumobots/2025/resources/2024 Sumobots Formal Rules.pdf"}
-                target="_blank"
-              >
-                2024 RULEBOOK
-              </Link>
+      <div className="min-h-screen w-full pt-12 bg-black/30 flex flex-col items-center">
+        <div className="max-w-5xl">
+          <section id="about" className="container">
+            <h2 className="font-display col-span-full">What is SUMOBOTS.</h2>
+            <div className="col-span-full flex flex-col-reverse items-center md:flex-row gap-8 mt-8">
+              <div className="md:w-[60%]">
+                <p>
+                  Sumobots is a sport in which two robots attempt to push each other
+                  out of a circle. You will be competing in a team of 1-6 people to
+                  build a robot that can defeat other bots. The competition is
+                  divided into the basic and the advanced stream, depending on your
+                  experience.
+                </p>
+                <Link
+                  className="button mt-8"
+                  href={"/sumobots/2025/resources/2024 Sumobots Formal Rules.pdf"}
+                  target="_blank"
+                >
+                  2024 RULEBOOK
+                </Link>
+              </div>
+              <div className="w-[70%] md:h-full md:w-[40%]">
+                <Image
+                  width={445}
+                  height={250}
+                  src={"/sumobots/2025/VR_005.gif"}
+                  alt="silly gif of robot"
+                  unoptimized={true}
+                ></Image>
+              </div>
             </div>
-            <div className="w-[70%] md:h-full md:w-[40%]">
-              <Image
-                width={445}
-                height={250}
-                src={"/sumobots/2025/VR_005.gif"}
-                alt="silly gif of robot"
-                unoptimized={true}
-              ></Image>
+          </section>
+          <section id="timeline" className="container">
+            <h2 className="col-span-full">Timeline.</h2>
+            <div className="col-span-full flex justify-center items-center mt-5">
+              <Timetable/>
             </div>
-          </div>
-        </section>
-        <section id="timeline" className="container">
-          <h2 className="col-span-full">Timeline.</h2>
-          <div className="col-span-full flex justify-center items-center mt-5">
-            <Timetable/>
-          </div>
-        </section>
-        <section id="sponsor" className="container pb-24">
-          <h2 className="col-span-full">Let&apos;s host a great event, together.</h2>
-          <p className="col-span-full">
-            Our sponsors allow us to host hundreds of students every year and provide them with great learning opportunities and fun experiences.<br />
-            If you would like to help us do the same this year, contact us at{" "}
-            <a className="text-link" href="mailto:industry@ramsocunsw.org">industry@ramsocunsw.org</a>, today!
-          </p>
-        </section>
+          </section>
+          <section id="sponsor" className="container pb-24">
+            <h2 className="col-span-full">Let&apos;s host a great event, together.</h2>
+            <p className="col-span-full">
+              Our sponsors allow us to host hundreds of students every year and provide them with great learning opportunities and fun experiences.<br />
+              If you would like to help us do the same this year, contact us at{" "}
+              <a className="text-link" href="mailto:industry@ramsocunsw.org">industry@ramsocunsw.org</a>, today!
+            </p>
+          </section>
+        </div>
       </div>
     </Fragment>
   );
