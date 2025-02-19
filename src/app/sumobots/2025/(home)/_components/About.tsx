fc4deecb5a3e858512132fa398bfa818f26c82ac
alanchoi00 <alanchoi.uni@gmail.com>
2025-02-19 12:54:58 +0800

    refactor(about): improve structure and styling of About component

--- src/app/sumobots/2025/(home)/_components/About.tsx
+++ src/app/sumobots/2025/(home)/_components/About.tsx
@@ -2,36 +2,36 @@ import Link from "next/link";
 import Image from "next/image";
 
 export const About = () => {
-    return (
-        <section id="about" className="container">
-            <h2 className="font-display col-span-full">What is SUMOBOTS.</h2>
-            <div className="col-span-full flex flex-col-reverse items-center md:flex-row gap-8 mt-8">
-              <div className="md:w-[60%]">
-                <p>
-                  Sumobots is a sport in which two robots attempt to push each other
-                  out of a circle. You will be competing in a team of 1-6 people to
-                  build a robot that can defeat other bots. The competition is
-                  divided into the basic and the advanced stream, depending on your
-                  experience.
-                </p>
-                <Link
-                  className="button mt-8"
-                  href={"/sumobots/2025/resources/2024 Sumobots Formal Rules.pdf"}
-                  target="_blank"
-                >
-                  2024 RULEBOOK
-                </Link>
-              </div>
-              <div className="w-[70%] md:h-full md:w-[40%]">
-                <Image
-                  width={445}
-                  height={250}
-                  src={"/sumobots/2025/VR_005.gif"}
-                  alt="silly gif of robot"
-                  unoptimized={true}
-                ></Image>
-              </div>
-            </div>
-          </section>
-    );
-}
+  return (
+    <section id="about" className="container">
+      <h2 className="font-display col-span-full">What is SUMOBOTS.</h2>
+      <div className="mt-8 flex flex-col-reverse items-center gap-8 md:flex-row">
+        <div className="flex flex-col gap-3 md:w-[60%]">
+          <p>
+            Sumobots is a sport in which two robots attempt to push each other
+            out of a circle. You will be competing in a team of 1-6 people to
+            build a robot that can defeat other bots. The competition is divided
+            into the basic and the advanced stream, depending on your
+            experience.
+          </p>
+          <Link
+            className="button"
+            href={"/sumobots/2025/resources/2024 Sumobots Formal Rules.pdf"}
+            target="_blank"
+          >
+            2024 RULEBOOK.
+          </Link>
+        </div>
+        <div className="w-[70%] md:h-full md:w-[40%]">
+          <Image
+            width={445}
+            height={250}
+            src={"/sumobots/2025/VR_005.gif"}
+            alt="silly gif of robot"
+            unoptimized={true}
+          ></Image>
+        </div>
+      </div>
+    </section>
+  );
+};
