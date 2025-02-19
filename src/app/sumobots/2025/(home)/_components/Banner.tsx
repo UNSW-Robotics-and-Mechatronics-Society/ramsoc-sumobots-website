a4cabc04e85fc392eb0e61521c415fe41f8079cf
alanchoi00 <alanchoi.uni@gmail.com>
2025-02-19 12:55:45 +0800

    style(banner): enhance layout and styling for improved readability and presentation

--- src/app/sumobots/2025/(home)/_components/Banner.tsx
+++ src/app/sumobots/2025/(home)/_components/Banner.tsx
@@ -4,36 +4,41 @@ export const Banner = () => {
   return (
     <div
       id="banner"
-      className="container flex h-[100vh] flex-col items-center justify-center gap-8"
+      className="container relative flex h-[100vh] min-h-[30rem] flex-col items-center justify-center"
     >
-      <div className="w-fit">
-        <h1 className="font-display col-span-full flex w-fit flex-col items-center leading-none">
+      <div className="absolute left-[50vw] top-1/2 -translate-x-1/2 -translate-y-1/4">
+        <h1 className="leading-none">
           <span className="self-start text-[0.8rem] md:text-[1rem] xl:text-[1.5rem]">
             RAMSOC UNSW PRESENTS
           </span>
-          <span className="xs:text[4rem] text-[3rem] sm:text-[5.5rem] md:text-[7rem] xl:text-[8rem]">
+          <span className="block text-[12vw] sm:text-[5.5rem] md:text-[7rem] xl:text-[8rem]">
             SUMOBOTS
           </span>
         </h1>
-        <div className="flex w-full flex-row justify-between pt-4">
-          <p className="text-[1rem] md:text-[1.5rem] xl:text-[1.7rem]">
-            2nd JUNE, <span className="text-rose-600">2025</span>
-          </p>
-          <p className="text-[1rem] md:text-[1.5rem] xl:text-[1.7rem]">
-            9 WEEKS EVENT
-          </p>
-        </div>
-        <div className="mt-7 flex h-fit w-full flex-col gap-2 bg-gray-800/50 p-4">
-          <h3 className="mb-0 text-[1.5rem] sm:text-[2rem]">
-            Join the robot battle.
-          </h3>
-          <p className="text-[0.8rem] md:text-[1rem] xl:text-[1.2rem]">
-            Applications open on May 25th. Fill out our EOI form to stay
-            updated.
-          </p>
-          <Link className="button text-[1rem]" href="/sumobots/2025/eoi">
-            EXPRESS INTEREST.
-          </Link>
+        <div className="flex w-full flex-col gap-4">
+          <div className="flex w-full flex-row justify-between pt-4">
+            <p className="pr-4 text-[1rem] md:text-[1.5rem] xl:text-[1.7rem]">
+              2nd JUNE, <span className="text-rose-600">2025</span>
+            </p>
+            <p className="text-right text-[1rem] md:text-[1.5rem] xl:text-[1.7rem]">
+              9 WEEKS EVENT
+            </p>
+          </div>
+          <div className="mt-7 flex w-full flex-col gap-3 bg-gray-800/50 p-4">
+            <h3 className="mb-0 text-[1rem] leading-normal sm:text-[2rem]">
+              Join the robot battle.
+            </h3>
+            <p>
+              Applications open on May 25th. Fill out our EOI form to stay
+              updated.
+            </p>
+            <Link
+              className="button-white bg-white text-[.7rem] leading-snug text-black sm:text-[1rem]"
+              href="/sumobots/2025/eoi"
+            >
+              EXPRESS INTEREST.
+            </Link>
+          </div>
         </div>
       </div>
     </div>
