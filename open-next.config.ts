413e231b993101c5abe165c7e11fbe5b493bfc79
alanchoi00 <alanchoi.uni@gmail.com>
2025-02-23 12:00:38 +0800

    feat(wrangler): transition to open-next/cloudflare

---
+++ open-next.config.ts
@@ -0,0 +1,24 @@
+// default open-next.config.ts file created by @opennextjs/cloudflare
+
+const config = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+};
+
+export default config;
