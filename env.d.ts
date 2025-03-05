5455f010c6f1f4ec72ea78dc3ea7982247cb4cab
alanchoi00 <alanchoi.uni@gmail.com>
2025-03-05 11:52:08 +0800

    feat: update environment variables to include SUMOBOTS_WORKER_KEY and remove unused env vars

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,6 @@
 declare namespace NodeJS {
-    export interface ProcessEnv {
-        GOOGLE_SHEETS_API_URL: string;
-        GOOGLE_SHEETS_API_TOKEN: string;
-        GOOGLE_SHEETS_SPREADSHEET_ID: string;
-        RECAPTCHA_SECRET_KEY: string;
-        NEXT_PUBLIC_RECAPTCHA_SITE_KEY: string;
-    }
+  export interface ProcessEnv {
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: string;
+    SUMOBOTS_WORKER_KEY: string;
+  }
 }
