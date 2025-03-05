683f97d973d1480e6ee2992c0cdf5cd094462e7e
alanchoi00 <alanchoi.uni@gmail.com>
2025-03-05 23:54:26 +0800

    feat: add Timetable component for workshop schedule display

---
+++ src/app/2025/(home)/workshop/Timetable.tsx
@@ -0,0 +1,46 @@
+import { TimelineData } from "../../_data/TimelineData";
+import { FaLocationDot } from "react-icons/fa6";
+
+interface TimetableProps {
+  className?: string;
+}
+
+export const Timetable = ({ className }: TimetableProps) => {
+  return (
+    <div className={`flex flex-col ${className}`}>
+      <h2>Timetable.</h2>
+      <p>
+        Join us for hands-on workshops to build and program your Sumobot! From
+        Arduino basics to advanced motor control, these sessions are for UNSW
+        students to learn, collaborate, and prepare for the competition. Check
+        the schedule below and get ready to bring your robot to life!
+      </p>
+      <div className={`mt-6 w-fit items-center justify-center self-center`}>
+        <div className="flex w-full flex-col space-y-4 sm:w-[35rem]">
+          {TimelineData.map((data, index) => (
+            <div
+              key={index}
+              className="flex items-center rounded-sm border-[1px] border-white border-opacity-10 bg-zinc-900/70"
+            >
+              <div className="flex h-full w-16 flex-shrink-0 flex-col justify-center border-r-[1px] border-white border-opacity-10 text-center text-lg font-semibold text-white text-opacity-70">
+                WK
+                <br />
+                {data.week}
+              </div>
+              <div className="flex flex-col justify-center p-2">
+                <div>{data.topics}</div>
+                <div className="flex flex-col gap-2 text-sm text-white text-opacity-70 sm:flex-row">
+                  {data.date}
+                  <span className="flex w-fit items-center justify-center gap-1 rounded-xl bg-zinc-800 px-2 font-semibold">
+                    <FaLocationDot size={12} />
+                    {data.location}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
