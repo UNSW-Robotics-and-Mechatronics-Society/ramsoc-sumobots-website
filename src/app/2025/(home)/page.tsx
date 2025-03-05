4ae1aa8290d837154b5eec1d5355c4b86cf07a37
alanchoi00 <alanchoi.uni@gmail.com>
2025-03-05 23:53:37 +0800

    feat: replace Timeline component with EventSchedule in home page

--- src/app/2025/(home)/page.tsx
+++ src/app/2025/(home)/page.tsx
@@ -2,7 +2,7 @@
 
 import { Fragment, useState } from "react";
 import { About } from "./_components/About/About";
-import { Timeline } from "./_components/Timeline";
+import { EventSchedule } from "./_components/EventSchedule";
 import { Sponsors } from "./_components/Sponsors";
 import Navbar from "./_components/Navbar";
 import { Banner } from "./_components/Banner";
@@ -17,7 +17,7 @@ export default function Page() {
       <div className="flex min-h-screen w-full flex-col items-center bg-black/30 pt-12">
         <div className="max-w-5xl">
           <About />
-          <Timeline />
+          <EventSchedule />
           <Sponsors />
           <Team />
         </div>
