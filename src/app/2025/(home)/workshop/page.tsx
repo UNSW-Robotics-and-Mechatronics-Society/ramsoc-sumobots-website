b26f1c663b7ef3fff24af351567647b8053a0e2d
alanchoi00 <alanchoi.uni@gmail.com>
2025-03-06 13:07:02 +0800

    feat: integrate Footer component into Workshop page and manage visibility state

--- src/app/2025/(home)/workshop/page.tsx
+++ src/app/2025/(home)/workshop/page.tsx
@@ -1,12 +1,15 @@
-import { Fragment } from "react";
+"use client";
+import { Fragment, useState } from "react";
 import Navbar from "../_components/Navbar";
 import { Timetable } from "./Timetable";
 import Resources from "./Resources";
+import Footer from "../_components/Footer";
 
 export default function Workshop() {
+  const [isFooterVisible, setFooterVisible] = useState(false);
   return (
     <Fragment>
-      <Navbar />
+      <Navbar isFooterVisible={isFooterVisible} />
       <section className="min-h-screen py-24">
         <div className="flex min-h-screen w-full flex-col items-center bg-black/30 pt-12">
           <div className="container flex flex-col">
@@ -19,6 +22,7 @@ export default function Workshop() {
           </div>
         </div>
       </section>
+      <Footer setFooterVisible={setFooterVisible} />
     </Fragment>
   );
 }
