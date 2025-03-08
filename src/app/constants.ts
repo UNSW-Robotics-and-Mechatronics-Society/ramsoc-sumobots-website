aa6cb54d50b81e3e11c1ae55c04ae06869c897b1
alanchoi00 <alanchoi.uni@gmail.com>
2025-03-08 19:28:53 +0800

    fix: add local development URL for SUMOBOTS worker site

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -6,3 +6,6 @@ export const SUMOBOTS_SITE_URL = "https://" + SUMOBOTS_DOMAIN;
 
 export const SUMOBOTS_WORKER_DOMAIN = "sumobots.worker." + MAIN_DOMAIN;
 export const SUMOBOTS_WORKER_SITE_URL = "https://" + SUMOBOTS_WORKER_DOMAIN;
+
+// Use this for local development
+// export const SUMOBOTS_WORKER_SITE_URL = "http://localhost:8787";
