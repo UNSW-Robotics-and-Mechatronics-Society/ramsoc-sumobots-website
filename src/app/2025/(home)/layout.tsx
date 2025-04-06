f1f8c870f96d99249fb770764c6ff27636575e2e
alanchoi00 <list>
2025-04-06 23:52:54 +1000

    feat: add metadataBase URL for RAMSoc Sumobots 2025

--- src/app/2025/(home)/layout.tsx
+++ src/app/2025/(home)/layout.tsx
@@ -1,7 +1,9 @@
 import { Metadata } from "next";
 import "@/app/styles.css";
+import { SUMOBOTS_SITE_URL } from "@/app/constants";
 
 export const metadata: Metadata = {
+  metadataBase: new URL(SUMOBOTS_SITE_URL),
   title: "RAMSoc Sumobots 2025",
   description:
     "Sumobots 2025 is here! Design, build, and compete in the most exciting robotics competition of the year. Sign up now!",
