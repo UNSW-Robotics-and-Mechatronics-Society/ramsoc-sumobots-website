d0bf486a74c49480093b633a310d57fcc358bc3a
alanchoi00 <list>
2025-04-07 10:29:14 +1000

    refactor(constants): update signup form URLs

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -11,5 +11,7 @@ export const SUMOBOTS_WORKER_SITE_URL = "https://" + SUMOBOTS_WORKER_DOMAIN;
 // export const SUMOBOTS_WORKER_SITE_URL = "http://localhost:8787";
 
 export const UNSW_ARC_SITE_URL = "https://www.arc.unsw.edu.au";
-export const OPEN_STREAM_FORM_URL =
-  "https://docs.google.com/forms/d/e/1FAIpQLSciKhBKAnkF2AwTzlVI-XyPJiT-bZjtOWi02eg3b2VT_WhKUg/viewform?usp=sharing";
+export const INTER_UNI_SIGNUP_FORM_URL =
+  "https://docs.google.com/forms/d/e/1FAIpQLSfA8iTPh5ygi-dtmcjNHmbqvvdDdGU6czRAzeix97Y15cKZAg/viewform";
+export const UNSW_ONLY_SIGNUP_FORM_URL =
+  "https://docs.google.com/forms/d/e/1FAIpQLSciKhBKAnkF2AwTzlVI-XyPJiT-bZjtOWi02eg3b2VT_WhKUg/viewform";
