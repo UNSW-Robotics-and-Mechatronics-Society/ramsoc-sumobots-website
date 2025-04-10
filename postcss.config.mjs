def0a0753cc57205bdc94b9ac7bc4ba11b65579b
alanchoi00 <list>
2025-04-10 11:27:52 +1000

    chore: simplify tailwindcss plugin configuration in postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
-import tailwindcss from "@tailwindcss/postcss";
 
 const config = {
   plugins: {
-    tailwindcss: tailwindcss("./tailwind.config.js"),
+    tailwindcss: {},
   },
 };
 
