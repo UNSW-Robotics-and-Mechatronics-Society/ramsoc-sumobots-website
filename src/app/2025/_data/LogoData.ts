6c86b03ce0de898dc7045befcf0f2bfbb9beca01
alanchoi00 <list>
2025-04-12 16:09:59 +1000

    chore: update LogoData to include new logos and descriptions for UNSW CSE, Mech School, and Engineers Australia

--- src/app/2025/_data/LogoData.ts
+++ src/app/2025/_data/LogoData.ts
@@ -1,16 +1,68 @@
-import { MAIN_SITE_URL, UNSW_ARC_SITE_URL } from "@/app/constants";
+import {
+  MAIN_SITE_URL,
+  UNSW_ARC_SITE_URL,
+  UNSW_CSE_SCHOOL_SITE_URL,
+  UNSW_MECH_SCHOOL_SITE_URL,
+} from "@/app/constants";
 
-export const LogoData = {
+export type LogoKey =
+  | "ramsoc"
+  | "unsw_arc_white"
+  | "unsw_arc_green_outline"
+  | "unsw_cse_school"
+  | "unsw_mech_school"
+  | "engineers_australia";
+
+export const LogoData: Record<
+  LogoKey,
+  {
+    label: string;
+    name: string;
+    image_url: string;
+    href: string;
+    bg_color: CSSStyleDeclaration["color"];
+  }
+> = {
   ramsoc: {
-    name: "ramsoc logo",
+    label: "ramsoc logo",
+    name: "RAMSOC",
     image_url: "/ramsoc_logo.svg",
     href: MAIN_SITE_URL,
+    bg_color: "black",
   },
-  arc: {
-    name: "unsw arc logo",
-    image_url: "/arc_logo.svg",
+  unsw_arc_white: {
+    label: "unsw arc logo white",
+    name: "UNSW Arc",
+    image_url: "/unsw_arc_logo_white.svg",
     href: UNSW_ARC_SITE_URL,
+    bg_color: "black",
   },
-};
-
-export type LogoKeys = keyof typeof LogoData;
+  unsw_arc_green_outline: {
+    label: "unsw arc logo green outline",
+    name: "UNSW Arc",
+    image_url: "/unsw_arc_logo_green_outline.svg",
+    href: UNSW_ARC_SITE_URL,
+    bg_color: "white",
+  },
+  unsw_cse_school: {
+    label: "unsw cse school logo",
+    name: "UNSW School of Computer Science and Engineering",
+    image_url: "/unsw_cse_school_logo.svg",
+    href: UNSW_CSE_SCHOOL_SITE_URL,
+    bg_color: "#FDDD00",
+  },
+  unsw_mech_school: {
+    label: "unsw mech school logo",
+    name: "UNSW School of Mechanical and Manufacturing Engineering",
+    image_url: "/unsw_mech_school_logo.svg",
+    href: UNSW_MECH_SCHOOL_SITE_URL,
+    bg_color: "#FDDD00",
+  },
+  engineers_australia: {
+    label: "engineers australia logo",
+    name: "Engineers Australia",
+    image_url: "/engineers_australia_logo.svg",
+    href: "https://www.engineersaustralia.org.au/",
+    bg_color: "white",
+  },
+} as const;
