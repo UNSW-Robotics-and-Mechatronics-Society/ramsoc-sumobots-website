0653ff877a1851932829ffcefef6bbd93d4e6ba3
alanchoi00 <list>
2025-04-15 10:56:32 +1000

    feat: add FAQ for SUMOBOTS 2025's prize

---
+++ src/app/2025/_data/faqs.ts
@@ -0,0 +1,39 @@
+export type Faq = {
+  question: string;
+  answer: string;
+};
+
+const faqs = [
+  {
+    question: "What's the prize for winners?",
+    answer:
+      "The prize pool for SUMOBOTS 2025 is over $2000! The exact prize breakdown for each category is currently being finalised.",
+  },
+  {
+    question: "How many team members do I need?",
+    answer:
+      "You can participate only in teams of 2 to 6 members. If you are interested in participating in the SUMOBOTS but do not have a team, we will assist you in connecting with other individual participants to form a team.",
+  },
+  {
+    question: "Who can participate in the competition?",
+    answer:
+      "Anyone can participate in the competition, but eligibility depends on the chosen stream. The Standard Stream is exclusively for UNSW students, while the Open Stream welcomes both UNSW and non-UNSW participants.",
+  },
+  {
+    question: "Do I need experience to participate?",
+    answer:
+      "No prior experience is necessary! We will host workshops to help you get started with building and programming your Sumobot.",
+  },
+  {
+    question: "How difficult is the competition?",
+    answer:
+      "The Sumobots Standard stream is ideal for complete beginners, and students looking to develop their technical skills. In our workshops we teach you all the skills you need to create a sumobot from scratch. In 2024, 5/8 quarter-finalist teams had no experience before sumobots!",
+  },
+  {
+    question: "How can I get involved?",
+    answer:
+      "Stay tuned for updates on our social media channels. We will announce the competition dates, workshops, and registration details soon!",
+  },
+];
+
+export default faqs;
