968931356d50eeb8dffd2c27a5add90eebd522dd
kevinsus <99kevin.wong99@gmail.com>
2025-05-23 13:22:44 +1000

    fix: stats doesnt display when initial load and support responsiveness

--- src/app/2025/(home)/_components/Stats/AnimatedCounter.tsx
+++ src/app/2025/(home)/_components/Stats/AnimatedCounter.tsx
@@ -1,8 +1,7 @@
 "user-client";
 
-import { useIsomorphicLayoutEffect, animate, useInView } from "framer-motion";
 import React from "react";
-import { motion, useMotionValue, useTransform } from "motion/react";
+import { animate, useMotionValue, useTransform, useInView } from "motion/react";
 
 type AnimatedCounterProps = {
   target: number;
@@ -13,25 +12,24 @@ const AnimatedCounter = ({ target, duration = 2 }: AnimatedCounterProps) => {
   const ref = React.useRef<HTMLSpanElement>(null);
   const inView = useInView(ref, { once: true }); // run only when ref is visible on screen
 
-  useIsomorphicLayoutEffect(() => {
-    const elemt = ref.current;
-    if (!elemt || !inView) return;
+  const count = useMotionValue(0);
+  const rounded = useTransform(() => Math.round(count.get()));
+  const [display, setDisplay] = React.useState(0);
 
-    const controls = animate(0, target, {
-      duration,
-      ease: "easeOut",
-      onUpdate(value) {
-        // Ensure integers
-        elemt.textContent = value.toFixed(0);
-      },
+  React.useEffect(() => {
+    if (!inView) return;
+
+    const controls = animate(count, target, { duration: duration });
+    const unsubscribe = rounded.on("change", (val) => {
+      setDisplay(val);
     });
-
     return () => {
       controls.stop();
+      unsubscribe();
     };
-  }, [ref, target, inView]);
+  }, [ref, inView]);
 
-  return <span ref={ref} />;
+  return <span ref={ref}>{display}</span>;
 };
 
 export default AnimatedCounter;
--- src/app/2025/(home)/_components/Stats/Stats.tsx
+++ src/app/2025/(home)/_components/Stats/Stats.tsx
@@ -9,46 +9,56 @@ import AnimatedCounter from "./AnimatedCounter";
 
 const Stats = () => {
   return (
-    <div className="mt-20 flex w-full justify-center gap-x-8 rounded-xl bg-gray-800/50 p-10">
-      <div className="flex flex-col items-center transition-all duration-300 hover:scale-105">
+    <div className="grid w-full grid-cols-1 gap-8 rounded-xl bg-gray-800/50 p-4 py-10 md:grid-cols-2 lg:grid-cols-4 lg:gap-4">
+      <div className="flex flex-col items-center justify-center transition-all duration-300 hover:scale-105">
         <div className="flex h-16 w-16 items-center justify-center rounded-full bg-white">
           <FaTrophy size={32} className="text-black" />
         </div>
-        <h2 className="my-4">
+        <h2 className="mt-4 mb-2">
           $<AnimatedCounter target={2300} />+
         </h2>
-        <p>Plus: A greener future</p>
-        <p>One tree planted every signup!</p>
+        <h3 className="text-center text-lg text-rose-600">Prizes Awarded</h3>
+        <p className="text-center">
+          Plus: A greener future One tree planted every signup!
+        </p>
       </div>
-      <div className="flex flex-col items-center transition-all duration-300 hover:scale-105">
+      <div className="flex flex-col items-center justify-center transition-all duration-300 hover:scale-105">
         <div className="flex h-16 w-16 items-center justify-center rounded-full bg-white">
           <FaGraduationCap size={32} className="text-black" />
         </div>
-        <h2 className="my-4">
+        <h2 className="mt-4 mb-2">
           <AnimatedCounter target={168} />
         </h2>
-        <p>Across all engineering</p>
-        <p>dispiplines</p>
+        <h3 className="text-center text-lg text-rose-600">
+          Students Participated
+        </h3>
+        <p className="text-center">Across all engineering dispiplines</p>
       </div>
-      <div className="flex flex-col items-center transition-all duration-300 hover:scale-105">
+      <div className="flex flex-col items-center justify-center transition-all duration-300 hover:scale-105">
         <div className="flex h-16 w-16 items-center justify-center rounded-full bg-white">
           <FaUsers size={32} className="text-black" />
         </div>
-        <h2 className="my-4">
+        <h2 className="mt-4 mb-2">
           <AnimatedCounter target={220} />+
         </h2>
-        <p>During Knockouts & Finals</p>
-        <p>of the competition</p>
+        <h3 className="text-center text-lg text-rose-600">Attendees</h3>
+        <p className="text-center">
+          During Knockouts & Finals of the competition
+        </p>
       </div>
-      <div className="flex flex-col items-center transition-all duration-300 hover:scale-105">
+      <div className="flex flex-col items-center justify-center transition-all duration-300 hover:scale-105">
         <div className="flex h-16 w-16 items-center justify-center rounded-full bg-white">
           <FaHandsHelping size={32} className="text-black" />
         </div>
-        <h2 className="my-4">
+        <h2 className="mt-4 mb-2">
           <AnimatedCounter target={40} />+
         </h2>
-        <p>Mechanical & Mechatronics</p>
-        <p>member workshops</p>
+        <h3 className="text-center text-lg text-rose-600">
+          Engineering Volunteers
+        </h3>
+        <p className="text-center">
+          Mechanical & Mechatronics member workshops
+        </p>
       </div>
     </div>
   );
