0bc376e0e23a4b3e11f38cd72d80be7850c21aec
Alan Choi <115921805+alanchoi00@users.noreply.github.com>
2025-06-06 19:34:25 +1000

    feat(workshop): update workshop timetable T2W1 (#135)
    * feat: update data structure and content for workshop timetable
    
    * fix: update topics and date formats in timetable
    
    * fix: improve destructuring in timetable date and location mapping https://github.com/UNSW-Robotics-and-Mechatronics-Society/ramsoc-sumobots-website/pull/135#discussion_r2131847989
    
    ---------
    
    Co-authored-by: Alan Choi <list>

--- src/app/2025/(home)/workshop/Timetable.tsx
+++ src/app/2025/(home)/workshop/Timetable.tsx
@@ -1,4 +1,4 @@
-import timelines from "@/app/2025/_data/timelines";
+import timetable from "@/app/2025/_data/timetable";
 import { FaLocationDot } from "react-icons/fa6";
 
 interface TimetableProps {
@@ -17,7 +17,7 @@ export const Timetable = ({ className }: TimetableProps) => {
       </p>
       <div className={`mt-6 w-fit items-center justify-center self-center`}>
         <div className="flex w-full flex-col space-y-4 sm:w-[35rem]">
-          {timelines.map((data, index) => (
+          {timetable.map((data, index) => (
             <div
               key={index}
               className="flex items-center rounded-xs border-[1px] border-white/10 bg-zinc-900/70"
@@ -27,14 +27,21 @@ export const Timetable = ({ className }: TimetableProps) => {
                 <br />
                 {data.week}
               </div>
-              <div className="flex flex-col justify-center p-2">
+              <div className="flex flex-col justify-center gap-1 p-2">
                 <div>{data.topics}</div>
-                <div className="text-opacity-70 flex flex-col gap-2 text-sm text-white sm:flex-row">
-                  {data.date}
-                  <span className="flex w-fit items-center justify-center gap-1 rounded-xl bg-zinc-800 px-2 font-semibold">
-                    <FaLocationDot size={12} />
-                    {data.location}
-                  </span>
+                <div className="flex flex-col gap-2">
+                  {data.date_and_location?.map(({ date, location }) => (
+                    <div
+                      key={`${date}-${location}`}
+                      className="text-opacity-70 flex flex-col gap-2 text-sm text-white sm:flex-row"
+                    >
+                      {date}
+                      <span className="flex w-fit items-center justify-center gap-1 rounded-xl bg-zinc-800 px-2 font-semibold">
+                        <FaLocationDot size={12} />
+                        {location}
+                      </span>
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
