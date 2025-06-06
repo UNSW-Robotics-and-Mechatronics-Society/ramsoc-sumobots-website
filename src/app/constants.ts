512c07c8255870eb6d3a0d8fbfe5aec8a94c118c
Alan Choi <115921805+alanchoi00@users.noreply.github.com>
2025-06-06 19:23:14 +1000

    feat(workshop): Update resource links for T2W1 (#136)
    * feat: Update resource links
    chore: remove outdated resource images and add new thumbnails for 2025
    
    * refactor: Remove redundant comment in resources.ts
    
    * fix: Remove outdated Arduino basics workshop URL
    
    ---------
    
    Co-authored-by: Alan Choi <list>

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -10,14 +10,38 @@ export const SUMOBOTS_WORKER_SITE_URL = "https://" + SUMOBOTS_WORKER_DOMAIN;
 // Use this for local development
 // export const SUMOBOTS_WORKER_SITE_URL = "http://localhost:8787";
 
+// Sponsors URLs
 export const UNSW_ARC_SITE_URL = "https://www.arc.unsw.edu.au";
 export const UNSW_CSE_SCHOOL_SITE_URL = "https://www.cse.unsw.edu.au/";
 export const UNSW_MECH_SCHOOL_SITE_URL =
   "https://www.unsw.edu.au/engineering/our-schools/mechanical-and-manufacturing-engineering";
 
+// Signup Form URLs
 export const INTER_UNI_SIGNUP_FORM_URL =
   "https://docs.google.com/forms/d/e/1FAIpQLSfA8iTPh5ygi-dtmcjNHmbqvvdDdGU6czRAzeix97Y15cKZAg/viewform";
 export const UNSW_ONLY_SIGNUP_FORM_URL =
   "https://docs.google.com/forms/d/e/1FAIpQLSciKhBKAnkF2AwTzlVI-XyPJiT-bZjtOWi02eg3b2VT_WhKUg/viewform";
-export const RULEBOOK_URL =
+
+// Resources URLs
+export const SUMOBOTS_RULEBOOK_URL =
   "https://docs.google.com/document/d/16KqDf9lArQZWStWT5tjy72ISZoLHtO71SLx5pW6Jmgw/edit?tab=t.0";
+export const SUMOBOTS_INFO_PACK_URL =
+  "https://www.canva.com/design/DAGh-iUsRKE/P4ZiyHShW1xyLvB6zs59KQ/watch?utlId=h75586c2a87";
+export const INTRO_WORKSHOP_SLIDES_URL =
+  "https://www.canva.com/design/DAGoc5Db0XA/5Zhigc_C92IRfbqMz7AbOg/view?utm_content=DAGoc5Db0XA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hefb91f5c09";
+
+export const ARDUINO_BASICS_VIDEO_URL =
+  "https://www.youtube.com/watch?v=Qaol1ywlcjQ";
+export const CAD_VIDEO_URL = "https://www.youtube.com/watch?v=2V1Y9ENvDSM";
+export const MOTOR_CONTROL_VIDEO_URL =
+  "https://www.youtube.com/watch?v=PFLQC4x5NoQ";
+export const PRINTING_VIDEO_URL = "https://www.youtube.com/watch?v=uO-_3uxiNsw";
+
+export const WORKSHOP_SAFETY_BADGE_URL =
+  "https://www.making.unsw.edu.au/access/badges/b/workshop-safety-badge/";
+export const LIPO_BATTERY_QUIZ_URL =
+  "https://docs.google.com/forms/d/e/1FAIpQLScSjGYCAzEFP_sGF4_SP_dDgeinQhSErrTiLSP2KeGhuRmtvg/viewform";
+export const ARDUINO_IDE_URL = "https://www.arduino.cc/en/software";
+export const CH340G_DRIVERS_URL =
+  "https://learn.sparkfun.com/tutorials/how-to-install-ch340-drivers/all";
+export const ONSHAPE_URL = "https://www.onshape.com/en/";
