b4f1b58e803d3228c7dd25035123318134029013
Alan Choi <list>
2025-06-07 20:52:26 +1000

    feat: update metadata for improved site description and icon handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import { Metadata } from "next";
 import "@/app/styles.css";
 import { SUMOBOTS_SITE_URL } from "@/app/constants";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   metadataBase: new URL(SUMOBOTS_SITE_URL),
   title: "RAMSoc Sumobots 2025",
   description:
-    "Sumobots 2025 is here! Design, build, and compete in the most exciting robotics competition of the year. Sign up now!",
-  icons: { icon: "ramsoc_logo.svg" },
+    "Sumobots 2025 is here! Design, build, and compete in the most exciting robotics competition of the year.",
+  icons: {
+    icon: [{ url: "/ramsoc_logo.svg", type: "image/svg+xml" }],
+  },
+  manifest: "/manifest.json",
   twitter: {
     card: "summary_large_image",
     title: "RAMSoc Sumobots 2025",
@@ -21,7 +23,7 @@ export const metadata: Metadata = {
     url: "https://sumobots.ramsoc.org/2025",
     title: "RAMSoc Sumobots 2025",
     description:
-      "Sumobots 2025 is here! Design, build, and compete in the most exciting robotics competition of the year. Sign up now!",
+      "Sumobots 2025 is here! Design, build, and compete in the most exciting robotics competition of the year.",
     siteName: "RAMSoc Sumobots 2025",
     images: "/2025/og.png",
   },
@@ -34,9 +36,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/ramsoc_logo.svg" />
-      </Head>
       <body>
         <main>{children}</main>
       </body>
