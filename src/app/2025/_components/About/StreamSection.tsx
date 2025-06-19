ddabac65644ba2475f2d3d7dcb421ee119772d12
Alan Choi <115921805+alanchoi00@users.noreply.github.com>
2025-06-19 19:57:23 +1000

    refactor: clean up specification table component (#145)
    * fix: update button text color to white for better visibility
    
    * fix: change topics display from div to h3 for semantic improvement
    
    * fix: update text color to white for better visibility in EventSchedule, Faq, and Navbar components
    
    * feat: refactor stream components and add RulebookBox for improved structure and clarity
    
    * fix: add flex display to button styles for improved alignment
    
    * fix: remove unnecessary class from paragraph in RulebookBox component
    
    ---------
    
    Co-authored-by: Alan Choi <list>

--- src/app/2025/_components/About/StreamSection.tsx
+++ src/app/2025/_components/About/StreamSection.tsx
@@ -4,6 +4,7 @@ import { AnimatePresence, motion, Variants } from "framer-motion";
 import { JSX, useState } from "react";
 import StandardStream from "./StandardStream";
 import OpenStream from "./OpenStream";
+import RulebookBox from "./RulebookBox";
 
 interface Tab {
   name: string;
@@ -24,6 +25,43 @@ const tabs: Tab[] = [
   },
 ];
 
+type TabSelectorProps = {
+  activeTab: Tab;
+  setActiveTab: (tab: Tab) => void;
+};
+
+const TabNavigation = ({ activeTab, setActiveTab }: TabSelectorProps) => {
+  return (
+    <div className="grid w-full grid-cols-2 gap-2 text-center">
+      {tabs.map((tab) => (
+        <button
+          key={tab.name}
+          onClick={() => setActiveTab(tab)}
+          className={`relative flex flex-col items-center justify-center gap-1 rounded-md px-6 py-3 transition duration-300 ${
+            activeTab.name === tab.name
+              ? "bg-gray-900 text-white shadow-lg"
+              : "hover:bg-gray-900/50 hover:text-gray-200"
+          }`}
+        >
+          <h4 className="text-lg font-semibold">{tab.name}</h4>
+          <p className="text-sm opacity-70">{tab.description}</p>
+
+          {/* Underline Indicator for Active Tab */}
+          {activeTab.name === tab.name && (
+            <motion.div
+              layoutId="tab-indicator"
+              className="absolute bottom-0 h-1 w-full rounded-md bg-rose-500"
+              initial={{ width: "0%" }}
+              animate={{ width: "100%" }}
+              transition={{ duration: 0.3 }}
+            />
+          )}
+        </button>
+      ))}
+    </div>
+  );
+};
+
 const tabContentVariants: Variants = {
   initial: { y: 10, opacity: 0 },
   enter: { y: 0, opacity: 1 },
@@ -35,41 +73,12 @@ const StreamSection = () => {
 
   return (
     <div className="mt-8 flex flex-col justify-center">
-      <h3 className="font-display self-center text-xl font-bold uppercase text-gray-100">
+      <h3 className="font-display self-center text-xl font-bold text-gray-100 uppercase">
         Select Your Path
       </h3>
 
-      {/* Tab Buttons */}
       <div className="mt-4 rounded-lg border border-white/10 bg-zinc-900/70 p-4 shadow-md">
-        <div className="grid w-full grid-cols-2 gap-2 text-center">
-          {tabs.map((tab) => (
-            <button
-              key={tab.name}
-              onClick={() => setActiveTab(tab)}
-              className={`relative flex flex-col items-center justify-center gap-1 rounded-md px-6 py-3 transition duration-300 ${
-                activeTab.name === tab.name
-                  ? "bg-gray-900 text-white shadow-lg"
-                  : "hover:bg-gray-900/50 hover:text-gray-200"
-              }`}
-            >
-              <h4 className="text-lg font-semibold">{tab.name}</h4>
-              <p className="text-sm opacity-70">{tab.description}</p>
-
-              {/* Underline Indicator for Active Tab */}
-              {activeTab.name === tab.name && (
-                <motion.div
-                  layoutId="tab-indicator"
-                  className="absolute bottom-0 h-1 w-full rounded-md bg-rose-500"
-                  initial={{ width: "0%" }}
-                  animate={{ width: "100%" }}
-                  transition={{ duration: 0.3 }}
-                />
-              )}
-            </button>
-          ))}
-        </div>
-
-        {/* Tab Content */}
+        <TabNavigation activeTab={activeTab} setActiveTab={setActiveTab} />
         <div className="mt-6">
           <AnimatePresence mode="wait">
             <motion.div
@@ -79,8 +88,10 @@ const StreamSection = () => {
               animate="enter"
               exit="exit"
               transition={{ duration: 0.3 }}
+              className="flex flex-col items-center justify-center gap-4"
             >
               {activeTab.render()}
+              <RulebookBox />
             </motion.div>
           </AnimatePresence>
         </div>
