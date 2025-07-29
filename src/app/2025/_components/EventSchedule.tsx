a3c49611c8b372987f6f978d3e75f9f0d2b63a1f
Kevin Susanto <112078697+kevinsus@users.noreply.github.com>
2025-07-29 19:39:03 +1000

    feat: Event Schedule section (#160)
    * added feature event schedule for KO + Finals
    
    * fix: added sponsors, and fix event page code style
    
    * fix: remove comments
    
    * fix: style

--- src/app/2025/_components/EventSchedule.tsx
+++ src/app/2025/_components/EventSchedule.tsx
@@ -1,7 +1,177 @@
-import Link from "next/link";
-import Social from "./Social";
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const EventSchedule = () => {
+  interface Tab {
+    name: string;
+    date: string;
+    place: string;
+    roundStatus: string;
+  }
+
+  interface Events {
+    time: string;
+    event: string;
+    details?: string;
+    showLogos?: boolean;
+  }
+
+  interface Sponsors {
+    source: string;
+    alt: string;
+    style: string;
+  }
+
+  const knockOutSchedule: Events[] = [
+    {
+      time: "15:00 PM",
+      event: "Opening remarks",
+      details:
+        "Team arrive, confirm attendance, collect bot stickers, practice run on the rings.",
+    },
+    {
+      time: "16:00 PM",
+      event: "Briefing",
+      details:
+        "Welcoming attendees, acknowledgment, Who We Are, and explain rules.",
+    },
+    {
+      time: "16:15 PM",
+      event: "Matches Start",
+      details: "Speed run matches",
+    },
+    {
+      time: "20:00 PM",
+      event: "Boss Battles",
+      details:
+        "Compete with bots created by projects / elec makerspace. Winner will get CSE + EA Merch !",
+    },
+    {
+      time: "20:30 PM",
+      event: "Closing",
+      details: "Announcement for Finals, wrap-up message, thank you",
+    },
+  ];
+
+  const finalsSchedule: Events[] = [
+    {
+      time: "16:45 PM",
+      event: "Opening Remarks",
+      details: "Confirm attendance, bot checking.",
+    },
+    {
+      time: "17:15 PM",
+      event: "Opening Presentation & Guest Speakers",
+      details:
+        "Welcoming attendees, acknowledgment, explain rules, guest speaker, introduce Pick-A-Bot !!",
+    },
+    {
+      time: "18:00 PM",
+      event: "Sumobot Matches (Finals)",
+      details: "Speed run matches",
+    },
+    {
+      time: "19:00 PM",
+      event: "Announcement",
+      details:
+        "Awards and announcing winners, group photos with winning teams, handout prizes.",
+    },
+    {
+      time: "19:20 PM",
+      event: "Industry Guest Speaker",
+      details: "Presentation by Dematic",
+    },
+    {
+      time: "19:35 PM",
+      event: "Industry Night & Networking",
+      details: "Companies:",
+      showLogos: true,
+    },
+    {
+      time: "21:15 PM",
+      event: "Official Event End",
+    },
+  ];
+
+  const sponsors: Sponsors[] = [
+    {
+      source: "/engineers_australia_logo.svg",
+      alt: "Engineers Australia",
+      style: "h-16 sm:h-25 w-auto",
+    },
+    {
+      source: "/bt_imaging_logo.png",
+      alt: "BT Imaging",
+      style: "h-10 sm:h-15 w-auto",
+    },
+    {
+      source: "/crest_robotics_logo.png",
+      alt: "CREST Robotics",
+      style: "h-10 sm:h-15 w-auto",
+    },
+    {
+      source: "/CSIRO_Logo.png",
+      alt: "CSIRO",
+      style: "h-16 sm:h-25 w-auto",
+    },
+    {
+      source: "/Dematic_logo.svg",
+      alt: "Dematic",
+      style: "h-10 sm:h-15 w-auto",
+    },
+    {
+      source: "/pearler_logo.png",
+      alt: "Pearler",
+      style: "h-10 sm:h-12 w-auto",
+    },
+    {
+      source: "/ResMed_logo.svg",
+      alt: "ResMed",
+      style: "h-12 sm:h-20 w-auto",
+    },
+    {
+      source: "/Rode_Microphones_logo.png",
+      alt: "Rode Microphones",
+      style: "h-16 sm:h-25 w-auto",
+    },
+  ];
+
+  const tabs: Tab[] = [
+    {
+      name: "Knock-Out Day",
+      date: "31 July 2025",
+      place: "Law Theatre G05",
+      roundStatus: "knockout",
+    },
+    {
+      name: "Finals Day",
+      date: "1 August 2025",
+      place: "Leighton Hall",
+      roundStatus: "finals",
+    },
+  ];
+  const [activeTab, setActiveTab] = React.useState(tabs[0]);
+
+  const scheduleData =
+    activeTab.roundStatus === "knockout" ? knockOutSchedule : finalsSchedule;
+
+  // Motions:
+  const containerVariants = {
+    hidden: { opacity: 0, y: 10 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { staggerChildren: 0.1, duration: 0.4 },
+    },
+    exit: { opacity: 0, y: -10, transition: { duration: 0.3 } },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, x: -20 },
+    visible: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: 20 },
+  };
+
   return (
     <section id="schedule" className="container">
       <h2 className="col-span-full">Event Schedule.</h2>
@@ -10,34 +180,73 @@ export const EventSchedule = () => {
         pushing their Sumobots to the limit in a test of strategy, speed, and
         control.
       </p>
-      <div className="col-span-full mt-5 flex h-[520px] items-center justify-center rounded-xl border border-dashed border-gray-500 p-6">
-        <div className="flex flex-col items-center justify-center rounded-md bg-gray-600/50 p-12 text-center">
-          <h2 className="mt-0">Yet to be disclosed.</h2>
-          <div className="max-w-96">
-            <p className="leading-relaxed">
-              Details are still being finalised—stay tuned!
-            </p>
-            <p>
-              UNSW students can also explore our{" "}
-              <Link
-                href={"/2025/workshop"}
-                className="underline-offset-2 hover:underline"
-              >
-                <b>workshops</b>
-              </Link>{" "}
-              to sharpen their skills before the competition.
-            </p>
-          </div>
-          <h3 className="leading-relaxed">Meanwhile,</h3>
-          <p>Follow us on our socials for our latest updates!</p>
-          <div className="mt-6 flex flex-col justify-center gap-4">
-            <div className="flex h-12 justify-center gap-8 text-white">
-              <Social socialName="instagram" size={32} />
-              <Social socialName="facebook" size={32} />
-              <Social socialName="discord" size={32} />
-            </div>
-          </div>
+
+      <div className="mx-auto mt-4 max-w-4xl rounded-lg border border-white/10 bg-zinc-900/70 p-4 shadow-md">
+        <div className="mb-10 flex justify-center gap-6">
+          {tabs.map((tab) => (
+            <button
+              key={tab.name}
+              onClick={() => setActiveTab(tab)}
+              className={`relative rounded-md px-8 py-3 font-semibold transition ${
+                activeTab.name === tab.name
+                  ? "bg-rose-600 text-white shadow-xl"
+                  : "text-gray-400 hover:bg-rose-600/30 hover:text-white"
+              }`}
+              aria-pressed={activeTab.name === tab.name}
+            >
+              {tab.name}
+
+              <span className="mt-1 block text-xs text-gray-300">
+                {tab.date} | {tab.place}
+              </span>
+            </button>
+          ))}
         </div>
+
+        <AnimatePresence mode="wait" initial={false}>
+          <motion.div
+            key={activeTab.roundStatus}
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className="space-y-8"
+          >
+            {scheduleData.map((item, index) => (
+              <motion.div
+                key={index}
+                className="flex divide-x-3"
+                variants={itemVariants}
+              >
+                <div className="flex w-2/10 justify-center">
+                  <p className="text-rose-600">{item.time}</p>
+                </div>
+                <div className="mx-10 w-8/10 pb-10">
+                  <p>
+                    <b>{item.event}</b>
+                  </p>
+                  <p className="text-gray-400">{item.details}</p>
+
+                  {/* Logo */}
+                  {item.showLogos && (
+                    <div className="relative mt-6 rounded-xl border border-white/10 bg-white/70 p-6 shadow-inner">
+                      <div className="flex flex-wrap items-center justify-center gap-6">
+                        {sponsors.map((item, index) => (
+                          <img
+                            key={index}
+                            src={item.source}
+                            alt={item.alt}
+                            className={item.style}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </motion.div>
+            ))}
+          </motion.div>
+        </AnimatePresence>
       </div>
     </section>
   );
